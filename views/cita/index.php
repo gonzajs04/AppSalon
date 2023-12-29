0981de11c5b36e125a004e9e5ca67d0995d77732
Gonzalo <hernandezgonzalo5845@gmail.com>
2023-12-29 00:58:47 -0300

    feat: se crearon para los pasos destinados a sacar una nueva cita. Style: se estilizaron los botones y el UI de citas

--- views/cita/index.php
+++ views/cita/index.php
@@ -45,4 +45,10 @@
         <p class="text-center">Verifica que la info sea correcta</p>
     </div>
 
+
+    <div class="paginacion">
+        <button id="anterior" class="boton">&laquo; Anterior</button>
+        <button id="siguiente" class="boton"> Siguiente &raquo;</button>
+    </div>
+
 </div>
