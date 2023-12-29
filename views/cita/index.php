0dd9be691ff45e9bf2ce256bde2f8fb9264c2e5d
Gonzalo <hernandezgonzalo5845@gmail.com>
2023-12-28 21:31:19 -0300

    style: estilizando endpoint cita index

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,7 +1,16 @@
-<h1 class="nombre-pagina">Crear nueva cita</h1>
-<p class="descripcion-pagina">Elige tus servicios a continuacion</p>
+<div class="container-titulo">
+    <h1 class="nombre-pagina">Crear nueva cita</h1>
+    <p class="descripcion-pagina movi">Elige tus servicios a continuacion</p>
+</div>
+
 
 <div class="app">
+    <nav class="tabs">
+        <button type="button" class="actual" data-paso="1">Servicios</button>
+        <button type="button"  data-paso="2">Informacion Cita</button>
+        <button type="button"  data-paso="3">Resumen</button>
+
+    </nav>
 
     <div id="paso-1" class="seccion">
         <h2>Servicios</h2>
@@ -34,7 +43,6 @@
     <div id="paso-3" class="seccion">
         <h2>Resumen</h2>
         <p class="text-center">Verifica que la info sea correcta</p>
-
     </div>
 
 </div>
