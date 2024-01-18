d2f1de7c8e62153ca8a861beab3219ae72ca0c61
gonzalo <hernandezgonzalo584@gmail.com>
2024-01-18 01:16:06 -0300

    feat: se agrega el nombre y la fecha a la cita. Asimismo se evalua si es sabado o domingo. Si es sabado o domingo la fecha se pone en rojo y sin asignar, de lo contrario en verde.

--- src/js/app.js
+++ src/js/app.js
@@ -24,7 +24,9 @@ function iniciarApp() {
 
     consumirApi(); //Consulta la API en el backend de PHP
 
+    nombreCliente(); //Almaceno en el objeto de cita el nombre del cliente
 
+    seleccionarFecha(); //Añade la fecha en el objeto de cita
 }
 
 async function consumirApi(){ //funcion asincrona debido a que necesitamos que se ejecuten otras funciones mientras se consulta la API
@@ -40,60 +42,6 @@ async function consumirApi(){ //funcion asincrona debido a que necesitamos que s
     //El try catch permite que la aplicacion siga funcionando aunque haya un ERROR. Consume mucha memoria, es lento. Se debe usar en partes criticas.
    
 }
-
-function mostrarServicios(servicios){
-    servicios.forEach(servicio =>{
-        const {id,nombre,precio} = servicio;
-
-        const nombreServicio = document.createElement('P');
-        nombreServicio.classList.add('nombre-servicio');
-        nombreServicio.textContent = nombre;
-
-        const precioServicio = document.createElement('P');
-        precioServicio.classList.add('precio-servicio');
-        precioServicio.textContent =  `$${precio}`;
-        
-        const servicioDiv = document.createElement('DIV');
-        servicioDiv.classList.add('servicio');
-        servicioDiv.dataset.idServicio = id; //le añado una id en especifico correspondiente al servicio a cada contenedor
-        //En el html se vera asi: data-id-servicio, quita el camelCase y pone guiones y minusculas
-       // servicioDiv.setAttribute('aria-label', `Servicio ${nombreServicio} con precio de ${precioServicio}`);
-
-
-        servicioDiv.appendChild(nombreServicio)
-        servicioDiv.appendChild(precioServicio)
-
-        document.querySelector('#servicios').appendChild(servicioDiv);
-
-        //Llenar el array de cita con el servicio clickeado.
-        //servicioDiv.onclick = seleccionarServicio; //No le pongo parentesis ya que si colocamos parentesis se llamara en todo momento esta funcion una vez clickee el usuario. Ahora, es necesario pasarle el servicio, como hacemos? Por medio de una funcion callback
-        servicioDiv.onclick = ()=>{
-            seleccionarServicio(servicio);
-            //servicioDiv.classList.toggle("seleccionado");
-        }
-        
-    } );
-}
-
-function seleccionarServicio(servicio){
-    const {id} = servicio;
-    const {servicios} = cita; //extraigo el array de servicios de el objeto de Cita
-   
-    //Comprobar si un servicio fue agregado o se quita
-    if(servicios.some(agregado => agregado.id === id)){ // Some Devuelve T o F
-        //Eliminarlo
-        cita.servicios = servicios.filter(agregado => agregado.id !== id); //Filtramos cual es diferente al que queremos eliminar para que quede en el carrito.
-
-    }else{
-        //Agregarlo
-        cita.servicios = [...servicios,servicio]; // Creo una copia al array de servicios y le agrego el servicio seleccionado. El array devuelto es almacenado en el array de servicios del objeto cita.
-    }
-
-    //Resaltar servicio seleccionado
-    const divServicio = document.querySelector(`[data-id-servicio="${id}"`);
-    divServicio.classList.toggle("seleccionado");
-}
-
 function mostrarSeccion() {
 
     // Ocultar la sección que tenga la clase de mostrar
@@ -174,3 +122,76 @@ function paginaSiguiente() {
     })
 }
 
+function mostrarServicios(servicios){
+    servicios.forEach(servicio =>{
+        const {id,nombre,precio} = servicio;
+
+        const nombreServicio = document.createElement('P');
+        nombreServicio.classList.add('nombre-servicio');
+        nombreServicio.textContent = nombre;
+
+        const precioServicio = document.createElement('P');
+        precioServicio.classList.add('precio-servicio');
+        precioServicio.textContent =  `$${precio}`;
+        
+        const servicioDiv = document.createElement('DIV');
+        servicioDiv.classList.add('servicio');
+        servicioDiv.dataset.idServicio = id; //le añado una id en especifico correspondiente al servicio a cada contenedor
+        //En el html se vera asi: data-id-servicio, quita el camelCase y pone guiones y minusculas
+       // servicioDiv.setAttribute('aria-label', `Servicio ${nombreServicio} con precio de ${precioServicio}`);
+
+
+        servicioDiv.appendChild(nombreServicio)
+        servicioDiv.appendChild(precioServicio)
+
+        document.querySelector('#servicios').appendChild(servicioDiv);
+
+        //Llenar el array de cita con el servicio clickeado.
+        //servicioDiv.onclick = seleccionarServicio; //No le pongo parentesis ya que si colocamos parentesis se llamara en todo momento esta funcion una vez clickee el usuario. Ahora, es necesario pasarle el servicio, como hacemos? Por medio de una funcion callback
+        servicioDiv.onclick = ()=>{
+            seleccionarServicio(servicio);
+            //servicioDiv.classList.toggle("seleccionado");
+        }
+        
+    } );
+}
+
+function seleccionarServicio(servicio){
+    const {id} = servicio;
+    const {servicios} = cita; //extraigo el array de servicios de el objeto de Cita
+   
+    //Comprobar si un servicio fue agregado o se quita
+    if(servicios.some(agregado => agregado.id === id)){ // Some Devuelve T o F
+        //Eliminarlo
+        cita.servicios = servicios.filter(agregado => agregado.id !== id); //Filtramos cual es diferente al que queremos eliminar para que quede en el carrito.
+
+    }else{
+        //Agregarlo
+        cita.servicios = [...servicios,servicio]; // Creo una copia al array de servicios y le agrego el servicio seleccionado. El array devuelto es almacenado en el array de servicios del objeto cita.
+    }
+
+    //Resaltar servicio seleccionado
+    const divServicio = document.querySelector(`[data-id-servicio="${id}"`);
+    divServicio.classList.toggle("seleccionado");
+}
+
+function nombreCliente(){
+    cita.nombre = document.querySelector("#nombre").value;
+}
+
+function seleccionarFecha(){
+    const fecha = document.querySelector('#fecha');
+    fecha.addEventListener('input',(e)=>{ //Se llama cuando cambia el valor del input en todo momento
+        //Evitar que el usuario seleccione una fecha que no se trabaja: Sabado o DOMINGO o ambos
+
+        const dia =  new Date(e.target.value).getUTCDay(); //Obtengo el numero del dia seleccionado
+        if([6,0].includes(dia)){ // Evaluo si es Sabado o Domingo
+            fecha.value = '';
+            fecha.style.background = "red";
+        }else{
+            cita.fecha = e.target.value;
+            fecha.style.background = "lightgreen";
+
+        }
+    })
+}
